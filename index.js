84c1188d36c72a355bb8f5763cabb02f0315469f
ikojun00 <ikojun00@fesb.hr>
2023-07-22 10:47:05 +0200

    rendering static files with express js

--- index.js
+++ index.js
@@ -1,39 +1,6 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-http.createServer((req,res)=>{
-    if(req.url==='/'){
-        fs.readFile('index.html', function(err, data) {
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type':'text/html'});
-            res.write(data);
-            return res.end;
-        });
-    }
-    else if(req.url==='/about'){
-        fs.readFile('about.html', function(err, data) {
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type':'text/html'});
-            res.write(data);
-            return res.end;
-        });
-    }
-    else if(req.url==='/contact-me'){
-        fs.readFile('contact-me.html', function(err, data) {
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type':'text/html'});
-            res.write(data);
-            return res.end;
-        });
-    }
-    else{
-        fs.readFile('404.html', function(err, data){
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type':'text/html'});
-            res.write(data);
-            return res.end;
-        });
-    }
-}).listen(8080,()=>{
-    console.log('Listening on port 8080.....');
-});
+app.use(express.static("public"))
+
+app.listen(8080)
